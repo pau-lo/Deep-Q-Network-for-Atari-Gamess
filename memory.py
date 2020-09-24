11025ee6aecda1f4c76c4a949e1a9080baa1d9b6
Paulo Lopez <pau_lo_lop_ez@protonmail.com>
2020-09-24 12:32:39 -0700

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import random as rand
 from collections import deque
 
@@ -10,11 +12,11 @@ class Memory:
 
     def __len__(self):
         return len(self.memory)
-        
+
     def add(self, state, action, reward, next_state, terminal):
         if len(self.memory) >= self.memory.maxlen:
             self.memory.popleft()
-        self.memory.append( (state, action, reward, next_state, terminal) )
+        self.memory.append((state, action, reward, next_state, terminal))
 
     def getSample(self):
         return rand.sample(self.memory, self.batch_size)
