d36da2d80e56bd1d65098213f94575078505669d
Paulo Lopez <pau_lo_lop_ez@protonmail.com>
2020-09-24 12:28:21 -0700

    Update dqn.py

--- dqn.py
+++ dqn.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import tensorflow as tf
 import random as rand
 import numpy as np
@@ -20,32 +22,38 @@ class DQN:
         self.history_length = params.history_length
         self.discount = params.discount
         self.eps = params.init_eps
-        self.eps_delta = (params.init_eps - params.final_eps) / params.final_eps_frame
+        self.eps_delta = (params.init_eps - params.final_eps) \
+            / params.final_eps_frame
         self.replay_start_size = params.replay_start_size
         self.eps_endt = params.final_eps_frame
         self.random_starts = params.random_starts
         self.batch_size = params.batch_size
-        self.ckpt_file = params.ckpt_dir+'/'+params.game
+        self.ckpt_file = params.ckpt_dir + '/' + params.game
 
         self.global_step = tf.Variable(0, trainable=False)
         if params.lr_anneal:
-            self.lr = tf.train.exponential_decay(params.lr, self.global_step, params.lr_anneal, 0.96, staircase=True)
+            self.lr = tf.train.exponential_decay(
+                          params.lr, self.global_step, params.lr_anneal, 0.96,
+                          staircase=True)
         else:
             self.lr = params.lr
 
         self.buffer = Buffer(params)
         self.memory = Memory(params.size, self.batch_size)
 
-        with tf.variable_scope("train") as self.train_scope:
+        with tf.variable_scope('train') as self.train_scope:
             self.train_net = ConvNet(params, trainable=True)
-        with tf.variable_scope("target") as self.target_scope:
+        with tf.variable_scope('target') as self.target_scope:
             self.target_net = ConvNet(params, trainable=False)
 
-        self.optimizer = tf.train.RMSPropOptimizer(self.lr, params.decay_rate, 0.0, self.eps)
+        self.optimizer = tf.train.RMSPropOptimizer(
+                                    self.lr, params.decay_rate, 0.0, self.eps)
 
-        self.actions = tf.placeholder(tf.float32, [None, self.num_actions])
+        self.actions = tf.placeholder(tf.float32, [None,
+                                                   self.num_actions])
         self.q_target = tf.placeholder(tf.float32, [None])
-        self.q_train = tf.reduce_max(tf.multiply(self.train_net.y, self.actions), reduction_indices=1)
+        self.q_train = tf.reduce_max(tf.multiply(
+                        self.train_net.y, self.actions), reduction_indices=1)
         self.diff = tf.subtract(self.q_target, self.q_train)
 
         half = tf.constant(0.5)
@@ -54,7 +62,8 @@ class DQN:
             clipped_diff = tf.clip_by_value(abs_diff, 0, 1)
             linear_part = abs_diff - clipped_diff
             quadratic_part = tf.square(clipped_diff)
-            self.diff_square = tf.multiply(half, tf.add(quadratic_part, linear_part))
+            self.diff_square = tf.multiply(half, tf.add(quadratic_part,
+                                                        linear_part))
         else:
             self.diff_square = tf.multiply(half, tf.square(self.diff))
 
@@ -64,7 +73,9 @@ class DQN:
             self.loss = tf.reduce_mean(self.diff_square)
 
         # backprop with RMS loss
-        self.task = self.optimizer.minimize(self.loss, global_step=self.global_step)
+
+        self.task = self.optimizer.minimize(self.loss,
+                                            global_step=self.global_step)
 
     def randomRestart(self):
         self.env.restart()
@@ -89,7 +100,8 @@ class DQN:
             a = rand.randrange(self.num_actions)
         else:
             x = self.buffer.getInput()
-            action_values = self.train_net.y.eval( feed_dict={ self.train_net.x: x } )
+            action_values = \
+                self.train_net.y.eval(feed_dict={self.train_net.x: x})
             a = np.argmax(action_values)
 
         state = self.buffer.getState()
@@ -105,29 +117,34 @@ class DQN:
 
         self.memory.add(state, action, reward, next_state, terminal)
 
-
-        return state, action, reward, next_state, terminal
+        return (state, action, reward, next_state, terminal)
 
     def doMinibatch(self, sess, successes, failures):
         batch = self.memory.getSample()
-        state = np.array([batch[i][0] for i in range(self.batch_size)]).astype(np.float32)
-        actions = np.array([batch[i][1] for i in range(self.batch_size)]).astype(np.float32)
-        rewards = np.array([batch[i][2] for i in range(self.batch_size)]).astype(np.float32)
-        successes += np.sum(rewards==1)
-        next_state = np.array([batch[i][3] for i in range(self.batch_size)]).astype(np.float32)
-        terminals = np.array([batch[i][4] for i in range(self.batch_size)]).astype(np.float32)
+        state = np.array([batch[i][0] for i in
+                         range(self.batch_size)]).astype(np.float32)
+        actions = np.array([batch[i][1] for i in
+                           range(self.batch_size)]).astype(np.float32)
+        rewards = np.array([batch[i][2] for i in
+                           range(self.batch_size)]).astype(np.float32)
+        successes += np.sum(rewards == 1)
+        next_state = np.array([batch[i][3] for i in
+                              range(self.batch_size)]).astype(np.float32)
+        terminals = np.array([batch[i][4] for i in
+                             range(self.batch_size)]).astype(np.float32)
 
-        failures += np.sum(terminals==1)
-        q_target = self.target_net.y.eval( feed_dict={ self.target_net.x: next_state } )
+        failures += np.sum(terminals == 1)
+        q_target = \
+            self.target_net.y.eval(feed_dict={self.target_net.x: next_state})
         q_target_max = np.argmax(q_target, axis=1)
-        q_target = rewards + ((1.0 - terminals) * (self.discount * q_target_max))
+        q_target = rewards + (1.0 - terminals) * (self.discount * q_target_max)
 
-        (result, loss) = sess.run( [self.task, self.loss],
-                                    feed_dict={ self.q_target: q_target,
-                                                self.train_net.x: state,
-                                                self.actions: actions } )
+        (result, loss) = sess.run([self.task, self.loss],
+                                  feed_dict={self.q_target: q_target,
+                                  self.train_net.x: state,
+                                  self.actions: actions})
 
-        return successes, failures, loss
+        return (successes, failures, loss)
 
     def play(self):
         self.randomRestart()
@@ -135,13 +152,17 @@ class DQN:
         for i in xrange(self.episodes):
             terminal = False
             while not terminal:
-                #aca cambie algo
-                state, action, reward, screen, terminal = self.observe(self.eps)
+
+                # aca cambie algo
+
+                (state, action, reward, screen, terminal) = \
+                    self.observe(self.eps)
 
     def copy_weights(self, sess):
         for key in self.train_net.weights.keys():
             t_key = 'target/' + key.split('/', 1)[1]
-            sess.run(self.target_net.weights[t_key].assign(self.train_net.weights[key]))
+            sess.run(self.target_net.weights[t_key].assign(
+                                                self.train_net.weights[key]))
 
     def save(self, saver, sess, step):
         saver.save(sess, self.ckpt_file, global_step=step)
